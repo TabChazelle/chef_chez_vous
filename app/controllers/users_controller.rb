a2be36f1a0887db575aab698afc076f88b28bac9
tabatha chazelle <tab_cha@yahoo.fr>
2024-01-29 17:02:28 +0100

    copied the controller code just in case

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def my_profile
+    @user = current_user
+    render :show
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to my_profile_path, notice: 'Your profile has been updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
