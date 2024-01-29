bb239806390031d504b24972abbe52d82c721d09
Elliot Marret <elliotmrt@icloud.com>
2024-01-29 16:36:27 +0100

    User and users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_many :bookings
+
+  validates :email, presence: true
+  validates :name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-has_many :bookings
 end
