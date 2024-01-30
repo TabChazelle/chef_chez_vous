d1f3e11bf73939380b327b654f344bd6d08032ab
Kai Tassie <argentiumkai@gmail.com>
2024-01-30 12:08:21 +0000

    Submitting new route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/index'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'users/new'
-  get 'users/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -20,6 +6,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :bookings, only: [:index, :show]
+  resources :chefs, except: [:destroy]
+  # Possibly needing to add resources for users if we are going to have a profile page.
+  # We do not need if it is only used for devise authentication.
+  resources :users, only: [:show, :edit, :update]
 
   # Defines the root path route ("/")
   # root "posts#index"
