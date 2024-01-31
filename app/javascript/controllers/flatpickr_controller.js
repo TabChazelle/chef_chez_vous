e618933b1925b0c53fc7a98ae13dbc98f3c225df
Kai Tassie <argentiumkai@gmail.com>
2024-01-31 18:39:38 +0000

    Completed Flatpickr

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
+
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  connect() {
+    console.log("connected", this.element);
+
+    flatpickr(".datepicker", {
+
+  })
+  }
+}
