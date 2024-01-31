79c0605486fb997f708b8a4a721cf27e247f909b
Brandon Schneider <bcschnei87@gmail.com>
2024-01-31 04:40:11 -0800

    bookings view pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :bookings, only: [:index, :show]
-  resources :chefs, only: [:new, :create, :show, :edit, :update]
+  resources :bookings, only: %i[new create show edit update index destroy]
+  resources :chefs, only: %i[new create show edit update]
   # Possibly needing to add resources for users if we are going to have a profile page.
   # We do not need if it is only used for devise authentication.
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: %i[show edit update]
   resources :users do
     get 'profile', on: :member
   end
