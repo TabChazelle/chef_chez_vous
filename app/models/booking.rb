f51f268f68101a33c7b47ed04d799d7814fddc3b
Elliot Marret <elliotmrt@icloud.com>
2024-02-01 17:30:25 +0100

    Booking details page done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :chef
+
+  def photo
+    # Replace this with the actual logic at a later date
+    "https://source.unsplash.com/random/#{self.id}"
+  end
+
 end
